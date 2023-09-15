c3704528eaa0ad77dab0971f39cc5e35df2be35b
TDI-180 <captainflyers11@gmail.com>
2023-09-16 00:38:30 +0200

    Clean-folder

--- box_namespaces/minibox/box.php
+++
@@ -1,8 +0,0 @@
-<?php
-	namespace App ;  
-	class box    {	
-							 public static function labox() {
-																			  echo 'c la box_0000..............verifier' ;
-																			}
-					   }
- ?>
